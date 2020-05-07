eaf9c77e9ac0629f18c6d56eef0ffa61083b3194
张文彬 <zhangwbin@yongyou.com>
2020-05-07 09:20:22 +0800

    递归解斐波那契

--- src/main/java/com/yonyou/letcode/Solution509.java
+++ src/main/java/com/yonyou/letcode/Solution509.java
@@ -22,7 +22,33 @@ public class Solution509 {
       cache[N] = 1;
       return 1;
     }
-    return fib(N-2) + fib(N-1);
+    int result = fib(N-2) + fib(N-1);
+    cache[N] = result;
+    return result;
   }
 
+  public int fib3(int N) {
+    int first = 0;
+    int second = 1;
+    if (N == 0) {
+      return first;
+    } else if (N == 1) {
+      return second;
+    } else {
+      int result = 0;
+      for (int i = 2; i <= N; i ++) {
+        result = first + second;
+        first = second;
+        second = result;
+      }
+      return result;
+    }
+  }
+  public static void main(String[] args) {
+
+    for (int i = 0; i <= 10; i++) {
+      Solution509 solution509 = new Solution509();
+      System.out.println(solution509.fib3(i));
+    }
+  }
 }
