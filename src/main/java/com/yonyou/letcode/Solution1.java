db024da2fd5716387c021521a57a2d792e291b1e
张文彬 <zhangwbin@yongyou.com>
2020-05-19 09:36:44 +0800

    两数之和

--- src/main/java/com/yonyou/letcode/Solution1.java
+++ src/main/java/com/yonyou/letcode/Solution1.java
@@ -2,9 +2,7 @@ package com.yonyou.letcode;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @Author: zhangwbin
@@ -24,7 +22,7 @@ public class Solution1 {
       }
     }
     return null;*/
-    // hash法
+    // hash法 时间复杂度O(n),空间复杂度O(n)
     Map<Integer, Integer> map = new HashMap<>();
     for (int i = 0; i< nums.length; i++) {
       int m = target - nums[i];
@@ -34,12 +32,34 @@ public class Solution1 {
       map.put(nums[i], i);
     }
     return null;
+
+   /* //两次遍历，加排序，使用hash存储结构，用来解决数据不重复时查询还可以
+    数据重复时查询并不能找到准确的下标
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int j = 0; j < nums.length; j++) {
+      map.put(nums[j], j);
+    }
+    Arrays.sort(nums);
+    for (int i = 0, j = nums.length - 1; i < j; ) {
+      if (nums[i] + nums[j] < target) {
+        i++;
+        continue;
+      }
+      if (nums[i] + nums[j] > target) {
+        j--;
+        continue;
+      }
+      if (nums[i] + nums[j] == target) {
+        return new int[]{map.get(nums[i]), map.get(nums[j])};
+      }
+    }
+    return null;*/
   }
 
   public static void main(String[] args) {
-    int[] nums = new int[]{2, 7, 11, 15};
+    int[] nums = new int[]{3, 2, 4};
     Solution1 solution1 = new Solution1();
-    int[] result = solution1.twoSum(nums, 9);
+    int[] result = solution1.twoSum(nums, 6);
     System.out.println(Arrays.toString(result));
   }
 
