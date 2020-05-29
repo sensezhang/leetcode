a7fdc69683dce9640744cbc7e8318865b51331b0
张文彬 <zhangwbin@yongyou.com>
2020-05-29 17:47:27 +0800

    二叉树

---
+++ src/main/java/com/yonyou/letcode/TreeNode.java
@@ -0,0 +1,15 @@
+package com.yonyou.letcode;
+
+/**
+ * @Author: zhangwbin
+ * @Date: 2020/5/29
+ */
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
