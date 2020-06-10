c9892fc75d7e4fc964a90609f7376eb506f1d802
张文彬 <zhangwbin@yongyou.com>
2020-06-10 16:48:44 +0800

    括号生成

---
+++ src/main/java/com/yonyou/letcode/Solution22.java
@@ -0,0 +1,43 @@
+package com.yonyou.letcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 括号生成
+ * @Author: zhangwbin
+ * @Date: 2020/6/10
+ */
+public class Solution22 {
+
+  List<String> result = new ArrayList<>();
+  public List<String> generateParenthesis(int n) {
+    if (n!=0) {
+      StringBuilder stringBuilder = new StringBuilder("(");
+      dfs(n - 1, n, stringBuilder);
+    }
+    return result;
+  }
+
+  private void dfs(int left, int right, StringBuilder stringBuilder) {
+    if (left == 0 && right == 0) {
+      result.add(stringBuilder.toString());
+    }
+    if (left > 0) {
+      stringBuilder.append("(");
+      dfs(left - 1, right, stringBuilder);
+      stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    }
+    if (right > left) {
+      stringBuilder.append(")");
+      dfs(left, right - 1, stringBuilder);
+      stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution22 solution22 = new Solution22();
+    List<String> result = solution22.generateParenthesis(1);
+    System.out.println(result);
+  }
+}
