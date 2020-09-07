e08dd63a7e29a8c9f9a026f7a25166a9c29a115d
张文彬 <zhangwbin@yongyou.com>
2020-09-07 09:58:36 +0800

    优先队列解决第K大的数组

---
+++ src/main/java/com/yonyou/letcode/priorityqueue/Solution347.java
@@ -0,0 +1,31 @@
+package com.yonyou.letcode.priorityqueue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * @Author: zhangwbin
+ * @Date: 2020/9/7
+ */
+public class Solution347 {
+
+  public int[] topKFrequent(int[] nums, int k) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int num : nums) {
+      Integer counter = map.getOrDefault(num, 0);
+      counter += 1;
+      map.put(num, counter);
+    }
+    PriorityQueue<int[]> queue = new PriorityQueue<>(k, (o1, o2) -> o2[1] - o1[1]);
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      int[] tmp = new int[]{entry.getKey(), entry.getValue()};
+      queue.offer(tmp);
+    }
+    int[] result = new int[k];
+    for (int i = 0; i < k; i++) {
+      result[i] = queue.poll()[0];
+    }
+    return result;
+  }
+}
